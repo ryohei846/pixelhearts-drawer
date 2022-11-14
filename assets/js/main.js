d495047650329530d8038675c2cdfaf45821141b
iida ryohei <ryohei012717@gmail.com>
2022-11-14 20:34:31 +0900

    commit at 2022/11/14

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,37 @@
 $(function () {
-  $('.menu-link').on('mouseover', function () {
-    $(this).addClass('active')
-  })
-  $('.menu-link').on('mouseout', function () {
-    $(this).removeClass('active')
-  })
+  function drawerAnimation() {
+    function toggleDrawer() {
+      $('.hamburger').toggleClass('active')
+      $('.close-button').toggleClass('active')
+      $('.drawer').toggleClass('active')
+      $('.filter').toggleClass('active')
+    }
+
+    function setEvent() {
+      $('.hamburger').on('click', function () {
+        toggleDrawer()
+      })
+      $('.close-button').on('click', function () {
+        toggleDrawer()
+      })
+      $('.hamburger').on('mouseover', function () {
+        $(this).addClass('mouseover')
+        $(this).removeClass('mouseout')
+      })
+      $('.hamburger').on('mouseout', function () {
+        $(this).removeClass('mouseover')
+        $(this).addClass('mouseout')
+      })
+      $('.menu-link').on('mouseover', function () {
+        $(this).addClass('mouseover')
+      })
+      $('.menu-link').on('mouseout', function () {
+        $(this).removeClass('mouseover')
+      })
+    }
+
+    setEvent()
+  }
+
+  drawerAnimation()
 })
